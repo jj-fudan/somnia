5d777001026ed8c083d1843ea34155e92e578a55
jiaojie <61367468+jj-fudan@users.noreply.github.com>
2020-03-07 10:55:21 +0800

    Update projects.scala

--- src/main/jjtest/spec/projects.scala
+++ src/main/jjtest/spec/projects.scala
@@ -11,10 +11,6 @@ class project_spec extends nv_somnia
 
     val PE_MAC_ATOMIC_K_SIZE = MAC_ATOMIC_K_SIZE/SPLIT_NUM
 
-    val PE_MAC_ATOMIC_C_SIZE_LOG2 = log2Ceil(PE_MAC_ATOMIC_C_SIZE)
-
-    val PE_MAC_ATOMIC_K_SIZE_LOG2 = log2Ceil(PE_MAC_ATOMIC_K_SIZE)
-
     val PE_MAC_RESULT_WIDTH = 2*SOMNIA_BPE + log2Ceil(PE_MAC_ATOMIC_C_SIZE)
 
 }
