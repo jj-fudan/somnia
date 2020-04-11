f38c69b67641cb3c10ec2eabe64a0ed43838a6e9
jiaojie <61367468+jj-fudan@users.noreply.github.com>
2020-04-11 01:18:27 -0700

    Add files via upload

--- src/test/scala/nvdla/cacc/SOMNIA_assembly_ctrl_Tests.scala
+++ src/test/scala/nvdla/cacc/SOMNIA_assembly_ctrl_Tests.scala
@@ -8,48 +8,52 @@ class SOMNIA_CACC_assembly_ctrlTests (c:SOMNIA_CACC_assembly_ctrl) extends PeekP
     //layer st
     //mac2accu
     val mac2accu_pd = "b000100000".U
-    
+    expect(c.io.wait_for_op_en,true)
+    poke(c.io.reg2dp_op_en,true)
+    step(1)
     //0
     poke(c.io.mac2accu_pd.valid,true)
     poke(c.io.mac2accu_pd.bits,mac2accu_pd)
-    poke(c.io.reg2dp_op_en,true)
     poke(c.io.reg2dp_clip_truncate,"b00010".U)
     poke(c.io.reg2dp_relu_bypass,true)
     poke(c.io.dp2reg_done,false)
     step(1)
-    //1
-    poke(c.io.mac2accu_pd.bits,"b000000000".U)
-    step(1)
-    //2 
-    //stripe end but not channel end
-    poke(c.io.mac2accu_pd.bits,"b000000000".U)
-    expect(c.io.wait_for_op_en,false)
-    expect(c.io.cfg_truncate,"b00010".U)
-    expect(c.io.cfg_relu_bypass,true)
-    expect(c.io.cfg_in_en_mask,true)
+    //0
     expect(c.io.abuf_rd_addr.valid,false)
     expect(c.io.abuf_rd_addr.bits,0)
-    expect(c.io.accu_ctrl_pd.valid,true)
-    expect(c.io.accu_ctrl_ram_valid,false)
-    expect(c.io.accu_ctrl_pd.bits,"b0000001000000".U)
-    expect(c.io.wait_for_op_en,false)
-    step(1)
+    expect(c.io.cfg_truncate,2)
+    expect(c.io.cfg_relu_bypass,true)
+    expect(c.io.accu_ctrl_pd.valid,false)
+    //1
     poke(c.io.mac2accu_pd.bits,"b001000000".U)
-    expect(c.io.abuf_rd_addr.bits,1)
+    step(1)
+    //0
+    expect(c.io.accu_ctrl_pd.valid,true)
     expect(c.io.accu_ctrl_pd.bits,"b0000001000000".U)
+    expect(c.io.accu_ctrl_ram_valid,false)
+    //1
+    expect(c.io.abuf_rd_addr.valid,false)
+    expect(c.io.abuf_rd_addr.bits,1)
+    expect(c.io.cfg_truncate,2)
+    expect(c.io.cfg_relu_bypass,true)
+    //2
+    poke(c.io.mac2accu_pd.bits,"b00010000".U)
     step(1)
-    expect(c.io.abuf_rd_addr.bits,2)
-    expect(c.io.accu_ctrl_pd.bits,"b0000001000001".U)
-    step(1)
+    //1
+    expect(c.io.accu_ctrl_pd.valid,true)
+    expect(c.io.accu_ctrl_pd.bits,"b0001001000001".U)
+    expect(c.io.accu_ctrl_ram_valid,false)
+    //2
     expect(c.io.abuf_rd_addr.valid,true)
     expect(c.io.abuf_rd_addr.bits,0)
-    expect(c.io.accu_ctrl_ram_valid,false)
-    expect(c.io.accu_ctrl_pd.bits,"b0001001000010".U)
+    expect(c.io.cfg_truncate,2)
+    expect(c.io.cfg_relu_bypass,true)
     step(1)
-    expect(c.io.accu_ctrl_ram_valid,false)
-    expect(c.io.accu_ctrl_pd.bits,"b0001001000000".U)
+    expect(c.io.accu_ctrl_pd.valid,true)
+    expect(c.io.accu_ctrl_pd.bits,"b0000001000000".U)
     step(1)
     expect(c.io.accu_ctrl_ram_valid,true)
+    
 }
 
 
